47fecce4ee795d455dc3d47b2dfea26627ae7549
Bharathikumar17 <bharathikumar17081996@gmail.com>
2017-07-04 17:05:55 +0530

    Update OneInteger.java

--- OneInteger.java
+++ OneInteger.java
@@ -1,7 +1,7 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class UniqueInteger {
+public class OneInteger {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
