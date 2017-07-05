463bfd54ffa2b84c48dc5ebcd283d4ef39d1ae19
Bharathikumar17 <bharathikumar17081996@gmail.com>
2017-07-05 18:19:06 +0530

    Create IntegerFromString.java

---
+++ IntegerFromString.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class IntegerFromString
+{
+    public static void main(String args[])
+    {
+        Scanner scan=new Scanner(System.in);
+        String s=scan.next();
+        System.out.print(Integer.parseInt(s));
+    }
+}
