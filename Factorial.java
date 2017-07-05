4a5304bb9d6663922071c43b7545853d262790c6
Bharathikumar17 <bharathikumar17081996@gmail.com>
2017-07-05 14:40:50 +0530

    Update Factorial.java

--- Factorial.java
+++ Factorial.java
@@ -2,16 +2,21 @@ import java.util.Scanner;
 public class Factorial {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		 	Scanner s=new Scanner(System.in);
+		Scanner s=new Scanner(System.in);
 	        int N=s.nextInt();
-	        int fact=1;
-	        if(N>1)
+	        if(N>=0)
 	        {
-	            for(int i=2;i<=N;i++)
-	            {
-	                fact=fact*i;
-	            }
+	        	int fact=1;
+	        	if(N>1)
+	        	{
+	        		for(int i=2;i<=N;i++)
+	        			fact=fact*i;
+	        	}
+	            System.out.print(fact);
 	        }
-	        System.out.print(fact);
+	        else if(N<0)
+	        	System.out.println("Invalid");
 	}
+
 }
+
